9d79a6e6064bddf228a7f35fda5e6f4c7b5e8679
Viktor Taraba <55559682+viktor-taraba@users.noreply.github.com>
2022-09-01 23:00:29 +0300

    Update Exercises.sql

--- Snowflake/Exercises.sql
+++ Snowflake/Exercises.sql
@@ -568,3 +568,52 @@ select GRADER(step, (actual = expected), actual, expected, description) as grade
   ,6 as expected
   ,'Check number of rows' as description
  ); 
+
+CREATE OR REPLACE TABLE LIBRARY_CARD_CATALOG.PUBLIC.NESTED_INGEST_JSON 
+(
+  "RAW_NESTED_BOOK" VARIANT
+);
+
+//a few simple queries
+SELECT RAW_NESTED_BOOK
+FROM NESTED_INGEST_JSON;
+
+SELECT RAW_NESTED_BOOK:year_published
+FROM NESTED_INGEST_JSON;
+
+SELECT RAW_NESTED_BOOK:authors
+FROM NESTED_INGEST_JSON;
+
+//try changing the number in the bracketsd to return authors from a different row
+SELECT RAW_NESTED_BOOK:authors[0].first_name
+FROM NESTED_INGEST_JSON;
+
+//Use these example flatten commands to explore flattening the nested book and author data
+SELECT value:first_name
+FROM NESTED_INGEST_JSON
+,LATERAL FLATTEN(input => RAW_NESTED_BOOK:authors);
+
+SELECT value:first_name
+FROM NESTED_INGEST_JSON
+,table(flatten(RAW_NESTED_BOOK:authors));
+
+//Add a CAST command to the fields returned
+SELECT value:first_name::VARCHAR, value:last_name::VARCHAR
+FROM NESTED_INGEST_JSON
+,LATERAL FLATTEN(input => RAW_NESTED_BOOK:authors);
+
+//Assign new column  names to the columns using "AS"
+SELECT value:first_name::VARCHAR AS FIRST_NM
+, value:last_name::VARCHAR AS LAST_NM
+FROM NESTED_INGEST_JSON
+,LATERAL FLATTEN(input => RAW_NESTED_BOOK:authors);
+
+-- Set your worksheet drop lists. DO NOT EDIT THE DORA CODE.
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (   
+     SELECT 'DWW17' as step 
+      ,(select row_count 
+        from LIBRARY_CARD_CATALOG.INFORMATION_SCHEMA.TABLES 
+        where table_name = 'NESTED_INGEST_JSON') as actual 
+      , 5 as expected 
+      ,'Check number of rows' as description  
+); 
